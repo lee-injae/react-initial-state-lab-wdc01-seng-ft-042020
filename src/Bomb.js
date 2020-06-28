6f8f5bef1fb705c46bfb3014ce8374e728149ab6
Injae Lee <github email address>
2020-06-27 23:57:10 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+import { render } from 'enzyme';
+
+class Bomb extends Component{
+
+        
+    constructor(props){
+        super()
+        this.state ={
+        secondsLeft: props.initialCount
+
+        }
+    }
+
+    render(){
+        let msg =  this.state.secondsLeft === 0 ? 
+        'Boom!' :
+        `${this.state.secondsLeft} seconds left before I go boom!`
+        return (
+            <div>
+               {msg}
+            </div>
+        )
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,26 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+
+    constructor(){
+        super()
+        this.state ={
+            currentSlideIndex: 0
+
+        }
+    }
+
+    render() {
+        return (
+            <h2>
+            I am on slide {this.state.currentSlideIndex}
+            </h2>
+        )
+    }
+
+}
+
+
+export default ImageSlider
